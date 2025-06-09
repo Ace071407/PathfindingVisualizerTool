3e6801e53f869b200bdc6fbfe2809de32c67eb27
Chander Rawat <163670562+Ace071407@users.noreply.github.com>
2025-06-09 17:52:48 +0530

    File uploads

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Pathfinder from "./components/pathfinder/Pathfinder";
+
+function App() {
+  return (
+    <div className="App">
+      <Pathfinder />
+    </div>
+  );
+}
+
+export default App;
